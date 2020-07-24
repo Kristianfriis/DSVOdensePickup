592017515060bae9121e6a67fb9860cf3aa02cd5
Kristian Friis <kristian.friis@dk.dsv.com>
2020-07-24 12:53:27 +0200

    .

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const slimauth = require('slimauth')
 const expressLayouts = require('express-ejs-layouts')
 const firebase = require('firebase')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 
